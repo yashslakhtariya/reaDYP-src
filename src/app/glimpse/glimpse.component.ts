760d3cc0440046f616536a4cc6b2e8c7f8f8eb0c
Dhruvi Suthar <dhruvihsuthar21@gnu.ac.in>
2023-11-26 21:18:43 +0530

    updated

--- src/app/glimpse/glimpse.component.ts
+++ src/app/glimpse/glimpse.component.ts
@@ -22,44 +22,4 @@ export class GlimpseComponent {
         });
     }
   }
-
-  authorName: string = '';
-  authorBooks: any[] = [];
-
-  constructor(private googleBooksService: GoogleBooksService) {}
-
-  onAuthorSearch() {
-    if (this.authorName) {
-      this.googleBooksService.searchBooksByAuthor(this.authorName)
-        .subscribe((data: any) => {
-          this.authorBooks = data.items || [];
-        });
-    }
-  }
-
-  authorName: string = '';
-  authorBooks: any[] = [];
-
-  constructor(private googleBooksService: GoogleBooksService) {}
-
-  onAuthorSearch() {
-    if (this.authorName) {
-      this.googleBooksService.searchBooksByAuthor(this.authorName)
-        .subscribe((data: any) => {
-          this.authorBooks = data.items || [];
-        });
-    }
-  }
-
-  constructor(private openaiService: OpenaiService) { }
-
-  book:string = '';
-  outputtxt: string = '';
-  generateText(data:string) {
-    this.openaiService.generateText(data).then(text => {
-      this.outputtxt = text + '';
-    }).catch(error => {
-      this.outputtxt = error;
-    })
-  }
 }
