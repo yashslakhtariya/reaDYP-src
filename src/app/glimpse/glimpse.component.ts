0546f026e73fd65e2dc06a38042acdb4ba68f43b
Yash Lakhtariya <yashslakhtariya21@gnu.ac.in>
2023-11-27 17:37:18 +0530

    add to wishlist in AuthorVerse

--- src/app/glimpse/glimpse.component.ts
+++ src/app/glimpse/glimpse.component.ts
@@ -1,5 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { GoogleBooksService } from '../google-books.service';
+import { WishlistService } from '../wishlist.service';
+import { SharedService } from '../shared.service';
 
 @Component({
   selector: 'app-glimpse',
@@ -7,12 +9,22 @@ import { GoogleBooksService } from '../google-books.service';
   styleUrls: ['./glimpse.component.css']
 })
 
-export class GlimpseComponent {
+export class GlimpseComponent implements OnInit {
 
   authorName: string = '';
   authorBooks: any[] = [];
+  username: any;
 
-  constructor(private googleBooksService: GoogleBooksService) {}
+  constructor(private googleBooksService: GoogleBooksService,
+    public sharedService: SharedService,
+    private wishlistService: WishlistService,
+  ) { }
+
+  ngOnInit() {
+    this.sharedService.username$.subscribe((username) => {
+      this.username = username;
+    });
+  }
 
   onAuthorSearch() {
     if (this.authorName) {
@@ -22,4 +34,22 @@ export class GlimpseComponent {
         });
     }
   }
+
+  addToWishlist(book: any) {
+    this.wishlistService.addToWishlist(this.username, book)
+      .subscribe({
+        next: (response: any) => {
+          console.log('Book added to wishlist:', response);
+          alert('Book added to wishlist!');
+        },
+        error: (error: any) => {
+          console.error('Error adding book to wishlist:', error);
+          alert('Error adding book to wishlist!');
+        },
+        complete: () => {
+          // Handle completion if needed
+        }
+      });
+  }
 }
+
